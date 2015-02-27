ca46d3ee5a36c4e799713a1c46ae479c3812acc3
João S. O. Bueno <gwidion@gmail.com>
2015-02-27 00:42:01 -0300

    Separate html into template

--- photolog.py
+++ photolog.py
@@ -1,33 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    html = """\
-<!DOCTYPE html>
-<html>
-    <body>
-        <ul>
-            <li>Cadastre-se</li>
-            <li>Entre</li>
-            <li>Subir fotos</li>
-            <li>Navegar</li>
-            <li>Sair</li>
-        </ul>
-
-        <h1>Bem-vindo ao Fotolog</h1>
-
-
-        <footer>
-            <p>Copyright 2015 - João S. O. Bueno</p>
-            <p>Feito com <a href="http://flask.pocoo.org/">Flask</a></p>
-        </footer>
-    </body>
-</html>\
-"""
-    return html
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run()
